53ff719e518863a8ce8a388312b34e9e1ed17aed
Manoj chauhan <mnojkumar0304@gmail.com>
2023-08-02 22:50:25 +0530

    Added diff.java

---
+++ diff.java
@@ -0,0 +1,7 @@
+public class diff{
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 2;
+        System.out.println("The difference of numbers is" + (a - b));
+    }
+}
